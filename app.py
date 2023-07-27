fb262e37af7322b1f609ed6624238bb3ab163312
Anish Gowda <anishgowda43@gmail.com>
2023-07-27 19:48:46 +0530

    added automatic qr detection in video

--- app.py
+++ app.py
@@ -1,59 +1,10 @@
 import traceback
 import streamlit as st
-from pyzbar import pyzbar
-from PIL import Image
 
 from libs.create_qr import CreateQr
 from libs.helper import qr_types,get_sidebar
 from libs.show_qr import ShowQr
-
-from segno import helpers as hp
-import cv2
-
-
-def qr_cam():
-    camera_id = 0
-    delay = 1
-    window_name = 'OpenCV QR Code'
-
-    qcd = cv2.QRCodeDetector()
-    cap = cv2.VideoCapture(camera_id)
-
-    while True:
-        ret, frame = cap.read()
-
-        if ret:
-            ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
-            if ret_qr:
-                for s, p in zip(decoded_info, points):
-                    if s:
-                        print(s)
-                        color = (0, 255, 0)
-                    else:
-                        color = (0, 0, 255)
-                    frame = cv2.polylines(frame, [p.astype(int)], True, color, 8)
-            cv2.imshow(window_name, frame)
-
-        if cv2.waitKey(delay) & 0xFF == ord('q'):
-            break
-
-    cv2.destroyWindow(window_name)
-
-
-
-def read_qr_code(upload):
-    try:
-        image = Image.open(upload)
-        qr_codes = pyzbar.decode(image)
-        qr_data = []
-        for qr in qr_codes:
-            data = qr.data.decode('utf-8')
-            qr_data.append(data)
-        print(qr_data)
-        return qr_data
-    except Exception:
-        traceback.print_exc()
-        return []
+from libs.read_qr import ReadQr
 
 
 def main():
@@ -83,19 +34,12 @@ def main():
                 st.warning("Cannot generate Qr code now")
     else:
         mode = st.radio("Choose Mode", ("Upload", "Camera"), horizontal=True)
+        rqr = ReadQr()
         if mode == "Upload":
-            upload = st.file_uploader("upload", ["png", "jpeg"])
+            rqr.decode_uploaded_qr()
         else:
-            upload = st.camera_input("Capture Image")
+            rqr.decode_from_video()
 
-        if upload is not None:
-            data = read_qr_code(upload)
-            if data:
-                st.subheader("QR Code Contents 😀😀😀")
-                for d in data:
-                    st.code(d)
-            else:
-                st.info("QR code cannot be decoded....😥😥😥")
 
 
 if __name__ == "__main__":
---
+++ libs/read_qr.py
@@ -0,0 +1,85 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
+from pyzbar import pyzbar
+import time
+
+class ReadQr:
+    def _get_boundry_box(self,np_image,qr_code,index):
+        qr_code_polygon = qr_code.polygon
+
+        # Draw a green boundary box around the detected QR code
+        pts = np.array(qr_code_polygon, np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(np_image, [pts], True, (0, 255, 0), 2)
+
+        # Draw the index above the boundary box
+        x, y, _, _ = cv2.boundingRect(pts)
+        cv2.putText(np_image, str(index), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        return np_image
+        
+
+    def decode_uploaded_qr(self):
+        upload = st.file_uploader("Upload an image", ["png", "jpeg", "jpg"], help="Upload an image with QR code")
+        if upload is not None:
+            image = Image.open(upload)
+            decoded_qr_codes = pyzbar.decode(image)
+
+            if decoded_qr_codes:
+                image_np = np.array(image)
+                st.subheader("QR Code Contents 😀😀😀")
+                img_place_holder = st.empty()
+                for i, qr_code in enumerate(decoded_qr_codes, start=1):
+
+                    image_np = self._get_boundry_box(np_image=image_np,qr_code=qr_code,index=i)
+
+                    st.code(f"{i} -> {qr_code.data.decode('utf-8')}")
+
+                image_with_boundary_boxes = Image.fromarray(image_np).convert("RGB")
+                img_place_holder.image(image_with_boundary_boxes)
+            else:
+                st.info("QR code cannot be decoded....😥😥😥")
+
+    def decode_from_video(self):
+        cap = cv2.VideoCapture(0)
+        frame_placeholder = st.empty()
+        stop_button_pressed = st.button("Stop")
+
+        start_time = time.time()
+        frame_count = 0
+        decode_frequency = 5  # Decode QR codes every 5 frames (adjust as needed)
+
+        while cap.isOpened() and not stop_button_pressed:
+            ret, frame = cap.read()
+            if not ret:
+                st.write("Video Capture Ended")
+                break
+
+            frame_count += 1
+            if frame_count % decode_frequency == 0:
+                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                decoded_qr_codes = pyzbar.decode(frame_rgb)
+
+                if decoded_qr_codes:
+                    st.subheader("QR Code Contents 😀😀😀")
+                    for i, qr_code in enumerate(decoded_qr_codes, start=1):
+                        st.code(f"{i} -> {qr_code.data.decode('utf-8')}")
+                        frame_rgb = self._get_boundry_box(np_image=frame_rgb, qr_code=qr_code, index=i)
+
+                    frame_placeholder.image(frame_rgb, channels="RGB")
+                    break
+
+            frame_placeholder.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), channels="RGB")
+
+            # Check if 30 seconds have elapsed and no QR code is detected
+            if time.time() - start_time > 30:
+                st.error("QR code not detected in 30 seconds. Please refresh the page to scan again")
+                break
+
+            if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
